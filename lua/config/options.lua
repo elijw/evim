110fa49c9b600f67245dea475d47a2ff5ccbc986
elijw <git@elijw.com>
2026-01-01 10:36:40 -0800

    fix invalid picker

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,4 +18,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.lazyvim_picker = "neotree"
+vim.g.lazyvim_picker = "telescope"
